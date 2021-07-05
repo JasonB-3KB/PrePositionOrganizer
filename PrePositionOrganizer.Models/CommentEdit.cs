9c3b0af0ab5d0d8fc4b29125e284b3c51085da5e
Jason Bagwill <mr.bagwill@gmail.com>
2021-07-05 16:56:17 -0400

    little fixes

--- PrePositionOrganizer.Models/CommentEdit.cs
+++ PrePositionOrganizer.Models/CommentEdit.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace PrePositionOrganizer.Models
     public class CommentEdit
     {
         public int CommentId { get; set; }
+        [Display(Name="Comment")]
         public string Text { get; set; }
         
     }
